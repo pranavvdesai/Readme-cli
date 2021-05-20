2cd8405fd8d4c0e1bb85e8cc7800ff3b59b934a6
pranav desai <74852751+pranavvdesai@users.noreply.github.com>
2021-05-21 04:41:10 +0530

    Update markdownModule.js

--- markdownModule.js
+++ markdownModule.js
@@ -1,28 +1,3 @@
-// module.exports = {markdownModule : (data)=>{
-//     return `     
-    
-//     # ${data.title}
-    
-//     ##Description
-//     ${data.description}
-    
-//     ##Overview
-//     ${data.overview}
-
-//     ##Usage
-//     ${data.usage}
-
-//     ##Authors
-//     ${data.authors}
-
-//     ##Contributors
-//     ${data.contributors}
-
-    
-//     `;} 
-
-// }
-
 
 function markdownModule(data){
     return `     
